a3ff0613e99758dd30d46cd863ff1592be737c02
Anthony Leclercq <aleclercq59.dev@gmail.com>
2023-12-05 21:42:37 +0100

    #T5 - Add Detail courses

---
+++ app/admin/courses/[courseId]/course.query.ts
@@ -0,0 +1,57 @@
+import { db } from "@/lib/prisma";
+
+export const getCourse = async ({
+  courseId,
+  userId,
+  userPage,
+}: {
+  courseId: string;
+  userId: string;
+  userPage: number;
+}) => {
+  const courses = await db.course.findUnique({
+    where: {
+      creatorId: userId,
+      id: courseId,
+    },
+    select: {
+      id: true,
+      image: true,
+      name: true,
+      presentation: true,
+      users: {
+        take: 5,
+        skip: Math.max(0, userPage * 5),
+        select: {
+          canceledAt: true,
+          id: true,
+          user: {
+            select: {
+              email: true,
+              image: true,
+              id: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          lessons: true,
+          users: true,
+        },
+      },
+    },
+  });
+
+  const users = courses?.users.map((user) => {
+    return {
+      canceled: user.canceledAt ? true : false,
+      ...user.user,
+    };
+  });
+
+  return {
+    ...courses,
+    users,
+  };
+};
