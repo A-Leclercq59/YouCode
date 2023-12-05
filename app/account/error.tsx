303f59c4e4e895c5711c61d8780e1640ff5b1197
Anthony Leclercq <aleclercq59.dev@gmail.com>
2023-12-05 23:21:47 +0100

    # T8 - Add my courses page

--- app/account/error.tsx
+++ app/account/error.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { useEffect } from "react";
 
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import LoginButton from "@/features/auth/LoginButton";
+import { NotAuthenticatedCard } from "@/features/errors/NotAuthentificatedCard";
 
 export default function Error({
   error,
@@ -15,14 +14,5 @@ export default function Error({
     console.error(error);
   }, [error]);
 
-  return (
-    <Card className="max-w-lg m-auto mt-4">
-      <CardHeader>
-        <CardTitle>You need to be logged in to view this page</CardTitle>
-      </CardHeader>
-      <CardFooter>
-        <LoginButton />
-      </CardFooter>
-    </Card>
-  );
+  return <NotAuthenticatedCard />;
 }
