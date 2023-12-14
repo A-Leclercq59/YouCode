7b544cd373284631f62105a04ca9e3e3caf2fda6
Anthony Leclercq <aleclercq59.dev@gmail.com>
2023-12-14 22:23:39 +0100

    # T19 - Add View Lesson

---
+++ app/courses/[courseId]/lessons/[lessonId]/lesson.query.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+
+export const getLesson = async (lessonId: string, userId = "-") => {
+  const lesson = await db.lesson.findUnique({
+    where: {
+      id: lessonId,
+      state: {
+        not: "HIDDEN",
+      },
+    },
+    select: {
+      id: true,
+      content: true,
+      name: true,
+      state: true,
+      users: {
+        where: {
+          id: userId,
+        },
+        select: {
+          id: true,
+          progress: true,
+        },
+      },
+    },
+  });
+
+  if (!lesson) {
+    return null;
+  }
+
+  return lesson;
+};
+
+export type LessonType = NonNullable<
+  Prisma.PromiseReturnType<typeof getLesson>
+>;
